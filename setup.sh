97ce16d200712905c69d18f701f85835d7a8e5b8
ArthurRL <arthur.lima@mettainnovations.com.br>
2024-05-19 23:26:48 -0300

    Added: some code

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ apt install build-essential vim curl ca-certificates gnupg lsb-release dpkg git 
                     libqt5multimedia5 libqt5multimedia5-plugins libqt5qml5 libqt5quick5 \
                     qml-module-qtquick2 xterm openssh-server libboost-all-dev libmbedtls-dev \
                     libcppunit-dev libxml2 libxml2-dev python3-dev wget gpg apt-transport-https \
-                    zsh zsh-autosuggestions zsh-syntax-highlighting code
+                    zsh zsh-autosuggestions zsh-syntax-highlighting
 
 # Add NVIDIA repository and install NVIDIA driver and CUDA
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
@@ -28,6 +28,14 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 apt update
 apt install nvidia-container-toolkit
+
+# Installing code 
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+apt update
+apt install code
 
 # Add Docker repository and install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
