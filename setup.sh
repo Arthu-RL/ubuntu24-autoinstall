28cf2a103c78172fddf8a0189d0858eda6940130
ArthurRL <111669311+Arthu-RL@users.noreply.github.com>
2024-05-27 15:39:14 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ apt update
 apt upgrade -y
 
 # Install necessary packages
-apt-get install -y build-essential vim curl ca-certificates gnupg lsb-release dpkg git \
+apt -y install build-essential vim curl ca-certificates gnupg lsb-release dpkg git \
                     qtdeclarative5-dev qml-module-qtquick-layouts qml-module-qtquick-controls \
                     qml-module-qtquick-controls2 qml-module-qtquick2 qml-module-qt-labs-settings \
                     qml-module-qtquick-dialogs libc6 libgcc-s1 libqt5core5a libqt5gui5 \
@@ -19,13 +19,13 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/
 dpkg -i cuda-keyring_1.1-1_all.deb
 apt update
 apt search nvidia-driver
-apt-get install -y nvidia-driver-550 cuda-drivers-550
+apt -y install nvidia-driver-550 cuda-drivers-550
 
 # Install NVIDIA container toolkit
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 apt update
-apt-get install -y nvidia-container-toolkit
+apt -y install nvidia-container-toolkit
 
 # Installing and fixing drivers dependencies
 ubuntu-drivers install -y
@@ -36,13 +36,13 @@ install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packa
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
 apt update
-apt-get install -y code
+apt -y install code
 
 # Add Docker repository and install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt -y install docker-ce docker-ce-cli containerd.io
 
 # Change the default shell to zsh
 export RUNZSH=no
@@ -50,7 +50,7 @@ export CHSH=no
 export KEEP_ZSHRC=yes
 
 # Install Oh My Zsh
-apt-get install -y zsh
+apt -y install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Installing plugins and theme
